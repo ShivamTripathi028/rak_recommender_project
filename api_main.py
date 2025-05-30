b00b28f202df60ae1c032eb5b6d1e3a978a7ec38
Shivam Tripathi <shivamamartripathi2004@gmail.com>
2025-05-30 15:45:21 +0530

    Origins modification to allow API calls from Netlify

--- api_main.py
+++ api_main.py
@@ -36,6 +36,7 @@ origins = [
     "http://127.0.0.1:8888",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:5173",
+    "https://shadow-talk-to-rak.netlify.app",
     # Add your deployed Netlify frontend URL here later like:
     # "https://your-unified-app-name.netlify.app"
 ]
